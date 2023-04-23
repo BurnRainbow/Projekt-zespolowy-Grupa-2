01a3cf65dbd76271c0549b75310e2fb6b6256a2c
BurnRainbow <seb0002@wp.pl>
2023-04-23 16:04:10 +0200

    Alpha 0.3.1.1 FIX index.html

--- src/Controller/ONasController.php
+++ src/Controller/ONasController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ONasController extends AbstractController
 {
-    #[Route('/api', name: 'app_o_nas')]
+    #[Route('/o-nas', name: 'app_o_nas')]
     public function index(): Response
     {
         return $this->render('o_nas/index.html.twig', [
