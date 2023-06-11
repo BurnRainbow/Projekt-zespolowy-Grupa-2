342480eb43b1965b68b188e51327001dc8fbbef9
Tygiel01 <skyrim2001@onet.pl>
2023-06-11 15:39:42 +0200

    Gra- karty+plansza

--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,9 @@
 "use strict"
 
 let Gra = {
-    'Gracz': {'Pojemnik': '#WynikGracza' , 'Wynik': 0},
+    'Gracz': {'Pojemnik': '#WynikGracza' , 'div':'#KartyGracz', 'Wynik': 0},
 
-    'Si': {'Pojemnik': '#WynikSi' , 'Wynik': 0},
+    'Si': {'Pojemnik': '#WynikSi' , 'div':'#KartySi', 'Wynik': 0},
     
     'Karty': ['2C','3C','4C','5C','6C','7C','8C','9C','10C','KC','QC','JC','AC','2D','3D','4D','5D','6D','7D','8D','9D','10D','KD','QD','JD','AD','2H','3H','4H','5H','6H','7H','8H','9H','10H','KH','QH','JH','AH','2S','3S','4S','5S','6S','7S','8S','9S','10S','KS','QS','JS','AS'],
     
@@ -20,11 +20,23 @@ const Si = Gra['Si'];
 function DobierzKarte(activeplayer) {
     const LiczbaLosowa = Math.floor(Math.random() * (Gra['Karty'].length));
     const ObecnaKarta = Gra['Karty'].splice(LiczbaLosowa, 1);
+
+    let katalog = "/js/";
+    let kartadowyswietlenia = ObecnaKarta;
+    let rozszerzenie = ".png";
+    let sciezka = katalog + kartadowyswietlenia + rozszerzenie;
+
+    let card = document.createElement('img');
+    card.src = sciezka;
+    
+    document.querySelector(activeplayer['div']).appendChild(card);
     
     AktualizujWynik(ObecnaKarta, activeplayer);
 
     PokazWynik(activeplayer);
     
+    //<img src="{{ asset('karty/9S.png') }}">
+    //<img src="2C.png">
 }
 
 //Akcja 1.2 /2.2
@@ -143,6 +155,16 @@ function Rozdaj(){
         alert('Najpierw zatwierdz swoje karty przed zakończeniem rozdania');
     }
     else{
+
+        let yourimg = document.querySelector('#KartyGracz').querySelectorAll('img');
+        let dealerimg = document.querySelector('#KartySi').querySelectorAll('img');
+        
+        for(let i=0; i<yourimg.length; i++){
+            yourimg[i].remove();
+        }
+        for(let i=0; i<dealerimg.length; i++){
+            dealerimg[i].remove();
+        }
 
     Gra['Karty'] = ['2C','3C','4C','5C','6C','7C','8C','9C','10C','KC','QC','JC','AC','2D','3D','4D','5D','6D','7D','8D','9D','10D','KD','QD','JD','AD','2H','3H','4H','5H','6H','7H','8H','9H','10H','KH','QH','JH','AH','2S','3S','4S','5S','6S','7S','8S','9S','10S','KS','QS','JS','AS'];
 
